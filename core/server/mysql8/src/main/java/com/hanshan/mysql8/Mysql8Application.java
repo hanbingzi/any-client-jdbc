8ef5f7364bb8993c07aee49bf9287d4a055f4af0
冷冰子 <lengbingzi@163.com>
2024-04-21 22:24:03 +0800

    项目初始化

---
+++ core/server/mysql8/src/main/java/com/hanshan/mysql8/Mysql8Application.java
@@ -0,0 +1,13 @@
+package com.hanshan.mysql8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mysql8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mysql8Application.class, args);
+    }
+
+}
